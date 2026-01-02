cb1c8bda21862c13b7c2050b029b674280a87e8f
fengqiu.dong@gmail.com <fengqiu.dong@gmail.com>
2026-01-02 08:04:27 -0500

    commiting new keyboard layout

--- keyboards/cosmotyl/config.h
+++ keyboards/cosmotyl/config.h
@@ -5,7 +5,7 @@
 #define BOOTMAGIC_COLUMN 0
 /* Keyboard matrix assignments */
 
-#define MATRIX_COL_PINS { GP12, GP11, GP10, GP9, GP8, GP7, GP6}
+#define MATRIX_COL_PINS { GP12, GP11, GP10, GP9, GP8, GP7, GP6} 
 #define MATRIX_ROW_PINS {GP0, GP2, GP3, GP4, GP5} //{ GP2, GP3, GP4, GP5, GP6, GP7, GP8 }
 
 #define MATRIX_COL_PINS_RIGHT { GP6, GP7, GP8, GP9, GP10, GP11, GP12 }
